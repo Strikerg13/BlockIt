4d53ee28b1e346037fe89fdb6bfa183bd36c7dc9
Matt Gillis <gillism@purdue.edu>
2018-11-20 18:51:52 -0500

    Finished the main code layout. There is a null reference error happening when a tetrad hits the ground, and disables itself. Looks like something is trying to access the tetradController script after it's disabled.

---
+++ BlockIt/Assets/Scripts/tetradScript.cs
@@ -0,0 +1,192 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tetradScript : MonoBehaviour {
+
+    float lastFall = 0;
+
+	// Use this for initialization
+	void Start () 
+    {
+        if (!isValidMatrixPos())
+        {
+            Debug.Log("GAME OVER");
+            Application.Quit();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+        // DISPLAY THE POSITION
+        //Debug.Log("x: " + (int)transform.position.x + " | y: " + (int)transform.position.y + " | z: " + (int)transform.position.z);
+
+
+
+		// LEFT
+        if (Input.GetAxis("Horizontal") < 0) // left
+        {   // move left 1 cell
+            transform.position += new Vector3(0, 0, 1);
+
+            // if it's a valid spot
+            if (isValidMatrixPos())
+            {   // save the move
+                updateTheMatrix();
+            }
+            else
+            {   // move it back
+                transform.position += new Vector3(0, 0, -1);
+            }
+        }
+
+        // RIGHT
+        else if (Input.GetAxis("Horizontal") > 0) // right
+        {   // move right 1 cell
+            transform.position += new Vector3(0, 0, -1);
+
+            // if it's a valid spot
+            if (isValidMatrixPos())
+            {   // save the move
+                updateTheMatrix();
+            }
+            else
+            {   // move it back
+                transform.position += new Vector3(0, 0, 1);
+            }
+        }
+
+        // FORWARD
+        else if (Input.GetAxis("Vertical") > 0) // down
+        {   // move forwad 1 cell
+            transform.position += new Vector3(1, 0, 0);
+
+            // if it's a valid spot
+            if (isValidMatrixPos())
+            {   // save the move
+                updateTheMatrix();
+            }
+            else
+            {   // move it back
+                transform.position += new Vector3(-1, 0, 0);
+            }
+        }
+
+        // BACKWARD
+        else if (Input.GetAxis("Vertical") > 0) // up
+        {   // move backward 1 cell
+            transform.position += new Vector3(-1, 0, 0);
+
+            // if it's a valid spot
+            if (isValidMatrixPos())
+            {   // save the move
+                updateTheMatrix();
+            }
+            else
+            {   // move it back
+                transform.position += new Vector3(1, 0, 0);
+            }
+        }
+
+        // ROTATE
+        else if (Input.GetButtonDown("Fire1")) // rotate button
+        {   // rotate the tetrad
+            transform.Rotate(0, 0, -90);
+
+            // if it's a valid spot
+            if (isValidMatrixPos())
+            {   // save the move
+                updateTheMatrix();
+            }
+            else
+            {   // rotate it back
+                transform.Rotate(0, 0, 90);
+            }
+        }
+
+        // FALL
+        else if (Time.time - lastFall >= 1) // change the 1 to adjust fall speed
+        {   // move down 1 cell
+            transform.position += new Vector3(0, -1,0);
+
+            // if it's a valid spot
+            if (isValidMatrixPos())
+            {   // save the move
+                updateTheMatrix();
+            }
+            else
+            {   // move it back
+                transform.position += new Vector3(1, 0, 0);
+                // clear any layers that are filled
+                theMatrix.deleteFullLayers();
+                // spawn the next tetrad
+                FindObjectOfType<spawnController>().spawnTetrad();
+                // disable this tetrad
+                //enabled = false;
+                Debug.Log("Deactivate");
+
+            }
+            // update timer variable
+            lastFall = Time.time;
+        }
+	}
+
+    // check if a cell in the matix is open for a block to move into
+    bool isValidMatrixPos()
+    {
+        // go through each block in the tetrad
+        foreach (Transform block in transform)
+        {
+            // save the block's position (rounded to whole numbers)
+            Vector3 v = theMatrix.roundVec3(block.position);
+
+            // if that position is not inside the play area
+            if (!theMatrix.insideBorder(v))
+            {   // it's invalid
+                return false;
+            }
+
+            // if there's already a block in that cell, and it's not part of this tetrad
+            if (theMatrix.matrix[(int)v.x, (int)v.y, (int)v.z] != null
+                && theMatrix.matrix[(int)v.x, (int)v.y, (int)v.z] != transform)
+            {   // it's invalid
+                return false;
+            }
+        }
+        // otherwise, it's a valid spot
+        return true;
+    }
+
+    void updateTheMatrix ()
+    {   // go through each cell in the play area
+        for (int y = 0; y < theMatrix.height; ++y)
+            for (int x = 0; x < theMatrix.width; ++x)
+                for (int z = 0; z < theMatrix.depth; ++z)
+                {   
+                    // if there's a block in that cell
+                    if (theMatrix.matrix[z, y, z] != null)
+                    {   // if the block is part of this tetrad
+
+
+                        // DISPLAY THE POSITION
+                        Debug.Log("x: " + x + " | y: " + y + " | z: " + z);
+
+                        if (theMatrix.matrix[x, y, z].parent == transform)
+                        {   // clear the block's spot in the matrix
+                            theMatrix.matrix[x, y, z] = null;
+                        }
+                    }
+                }
+
+        foreach (Transform block in transform)
+        {
+            // save the block's position (rounded to whole numbers)
+            Vector3 v = theMatrix.roundVec3(block.position);
+            // add the block back into the matrix ??????????????????????????????
+            theMatrix.matrix[(int)v.x, (int)v.y, (int)v.z] = block;
+        }
+    }
+
+
+}
--- BlockIt/Assets/Scripts/theMatrix.cs
+++ BlockIt/Assets/Scripts/theMatrix.cs
@@ -7,7 +7,7 @@ public class theMatrix : MonoBehaviour {
 	//Define the matrix
     public static int width = 6; // X-axis
     public static int depth = 6; // Z-axis
-    public static int height = 13; // Y-axis
+    public static int height = 14; // Y-axis + 1 to make room for spawner
 
     public static Transform[,,] matrix = new Transform[width,height,depth];
 
@@ -20,6 +20,88 @@ public class theMatrix : MonoBehaviour {
     // determine if a coordinate is within the borders of the matrix
     public static bool insideBorder(Vector3 pos)
     {
-        
+        return ((int)pos.x >= 0 && (int)pos.x < width  // check x boundaries
+            && (int)pos.y > 0                         // falling, so check if y is above the ground
+            && (int)pos.z >= 0 && (int)pos.z < depth); // check z boundaries
+    }
+
+    // Delete all the blocks in a single layer
+    public static void deleteLayer (int y)
+    {
+        // loop through each cell on the x/z plane & destroy the block there
+        for (int x = 0; x < width; ++x)
+        {
+            for (int z = 0; z < depth; ++z)
+            {
+                // destroy the block at that position
+                Destroy(matrix[x, y, z].gameObject);
+                // update the matrix to make that cell empty
+                matrix[x, y, z] = null;
+            }
+        }
+    }
+
+    // Shift all blocks in a specified layer down one layer
+    public static void decreaseLayer (int y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            for (int z = 0; z < depth; ++z)
+            {
+                if (matrix[x, y, z] != null)
+                {
+                    // Move the block down 1 layer in the matrix
+                    matrix[x, y-1, z] = matrix[x, y,z];
+                    // update the cell we just moved from to empty in the matrix
+                    matrix[x,y,z] = null;
+                    // move the actual block down
+                    matrix[x, y-1, z].position += new Vector3(0,-1,0);
+                }
+            }
+        }
+    }
+
+    // lower all the layers above a specified layer
+    public static void decreaseLayersAbove (int y)
+    {
+        // loop through each layer above the deleted layer
+        for (int i = y; i < height; ++i)
+        {
+            // shift the layer down 1
+            decreaseLayer(i);
+        }
+    }
+
+    // check if a layer has blocks in every cell
+    public static bool isLayerFull (int y)
+    {
+        // loop through each cell in the layer & check if it's empty
+        for (int x = 0; x < width; ++x)
+        {
+            for (int z = 0; z < depth; ++z)
+            {
+                if (matrix[x, y, z] == null)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // Perform the full action of deleting a layer
+    public static void deleteFullLayers ()
+    {
+        for (int y = 0; y < height; ++y)
+        {
+            if (isLayerFull(y)) // if the layer is filled
+            {
+                deleteLayer(y);             // delete that layer
+                decreaseLayersAbove(y + 1); // shift all the layers above it down
+
+                // go through this layer again to catch the layer that was just shifted down here
+                --y; 
+            }
+        }
     }
 }
